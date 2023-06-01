2ce64de7c4269e8e3f77b44004cf8270d23991c4
Nalnalyanto <106339798+Nalnalyanto@users.noreply.github.com>
2023-06-01 12:09:05 +0700

    Update siamese_network.py
    Menambahkan unit dan layer batch normalization

--- siamese_network.py
+++ siamese_network.py
@@ -1,32 +1,39 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Lambda, Input
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Lambda, Input, Dropout, BatchNormalization
 from tensorflow.keras.models import Model
 
 def buat_siamese(input_shape, embedding_dim=128):
     inp = Input(input_shape)
     
     #first convo
-    c = Conv2D(4, (3,3), activation='relu', padding='same')(inp)
-    m = MaxPooling2D(2,2)(c)
+    c = Conv2D(4, (3,3), padding='same')(inp)
+    n = BatchNormalization()(c)
+    a = tf.keras.layers.Activation('relu')(n)
+    m = MaxPooling2D((2,2), padding='same')(a)
     
     #second convo
-    c = Conv2D(8, (3,3), activation='relu')(m)
-    m = MaxPooling2D(2,2)(c)
+    c = Conv2D(8, (3,3), padding='same')(m)
+    n = BatchNormalization()(c)
+    a = tf.keras.layers.Activation('relu')(n)
+    m = MaxPooling2D((2,2), padding='same')(a)
     
     #third convo
-    c = Conv2D(16, (3,3), activation='relu')(m)
-    m = MaxPooling2D(2,2)(c)
-    
+    c = Conv2D(16, (3,3), padding='same')(m)
+    n = BatchNormalization()(c)
+    a = tf.keras.layers.Activation('relu')(n)
+    m = MaxPooling2D((2,2), padding='same')(a)
+       
     #fourt convo
-    c = Conv2D(64, (3,3), activation='relu')(m)
-    m = MaxPooling2D(2,2)(c)
+    c = Conv2D(32, (3,3), padding='same')(m)
+    n = BatchNormalization()(c)
+    a = tf.keras.layers.Activation('relu')(n)
+    m = MaxPooling2D((2,2), padding='same')(a)
     
-    #fifth convo
-    c = Conv2D(64, (3,3), activation='relu')(m)
-    m = MaxPooling2D(2,2)(c)
+    #c = Conv2D(128, (3,3), activation='relu')(m)
     
     #fc
     f = Flatten()(m)
-    d = Dense(64, activation='relu')(f)
-    output = Dense(embedding_dim)(f)
+    d = Dense(1024, activation='relu')(f)
+    drop = Dropout(0.2)(d)
+    output = Dense(embedding_dim)(drop)
     return Model(inputs=inp , outputs=output)
